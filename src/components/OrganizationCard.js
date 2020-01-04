050764e9c969fe0498b771906ef379f8f55bbb38
Taljjaa <jenken131@gmail.com>
2020-01-04 15:41:34 -0800

    refactored OrganizationCard to functional component with useNetworkResource

--- src/components/OrganizationCard.js
+++ src/components/OrganizationCard.js
@@ -1,76 +1,41 @@
 import React from 'react';
 import { Segment } from 'semantic-ui-react';
 import EventCard from './EventCard';
+import useNetworkResource from '../hooks/useNetworkResource';
 
-class OrganizationCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      organization: [],
-    };
+const OrganizationCard = ({ match }) => {
+  const [organization, fetched] = useNetworkResource(
+    `http://localhost:3000/organizations/${match.params.id}`,
+  );
+
+  if (!fetched) {
+    return null;
   }
 
-  componentDidMount = async () => {
-    console.log('mounting');
-    const organizationFetch = await fetch(
-      `http://localhost:3000/organizations/${this.props.match.params.id}`,
-    );
-    const organizationJson = await organizationFetch.json();
-    console.log(organizationJson, 'json');
-    this.setState({
-      organization: organizationJson,
-    });
-  };
+  const { display_name, address, logo, description, mission, contact, events } = organization;
 
-  createEventCards = events => {
-    console.log(events);
-    if (events) {
-      return events.map(event => {
-        const testEvent = {
-          id: event.id,
-          title: event.title,
-          description: event.description,
-          location: event.location,
-          organization: {
-            id: this.state.organization.id,
-            logo: this.state.organization.logo,
-            display_name: this.state.organization.display_name,
-          },
-        };
-        console.log(event);
-        return <EventCard key={event.id} event={testEvent} />;
-      });
-    }
-  };
-
-  render() {
-    console.log('rendering');
-    const {
-      display_name,
-      address,
-      logo,
-      description,
-      mission,
-      contact,
-      events,
-    } = this.state.organization;
-    console.log(events);
-    return (
-      <div>
-        <Segment.Group>
-          <Segment>
-            <img src={logo} alt={`${display_name}'s logo`} />
-          </Segment>
-          <Segment>{display_name}</Segment>
-          <Segment>{mission}</Segment>
-          <Segment>{address}</Segment>
-          <Segment>{description}</Segment>
-          <Segment>{contact}</Segment>
-        </Segment.Group>
-        {this.createEventCards(events)}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Segment.Group>
+        <Segment>
+          <img src={logo} alt={`${display_name}'s logo`} />
+        </Segment>
+        <Segment>{display_name}</Segment>
+        <Segment>{mission}</Segment>
+        <Segment>{address}</Segment>
+        <Segment>{description}</Segment>
+        <Segment>{contact}</Segment>
+      </Segment.Group>
+      {events &&
+        events
+          .map(event => ({
+            ...event,
+            organization,
+          }))
+          .map(event => <EventCard key={event.id} event={event} />)}
+      )}
+    </div>
+  );
+};
 
 export default OrganizationCard;
